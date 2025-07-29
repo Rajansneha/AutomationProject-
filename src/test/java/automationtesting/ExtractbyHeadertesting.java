be5d6d048ab3d462ce0c832ff29778b93a6fd02f
Sneha Rajan <sneharajan6384@gmail.com>
2025-07-29 19:00:09 +0530

    Initial commit

---
+++ src/test/java/automationtesting/ExtractbyHeadertesting.java
@@ -0,0 +1,42 @@
+package automationtesting;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class ExtractbyHeadertesting {
+
+	public static void main(String[] args) {
+		//launching the app
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/");
+		//login
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        //Sorting option 
+        WebElement dropdowmoption = driver.findElement(By.className("product_sort_container"));
+        Select filter = new Select(dropdowmoption);
+        filter.selectByVisibleText("Name (Z to A)");
+        //taking product and the price
+        List<WebElement> productTitles = driver.findElements(By.className("inventory_item_name"));
+        List<WebElement> productPrices = driver.findElements(By.className("inventory_item_price"));
+        //getting the three minimum product
+        int count = Math.min(3, Math.min(productTitles.size(), productPrices.size()));
+        System.out.println("Top Products (Name Z to A):");
+        for (int i = 0; i < count; i++) {
+            String title = productTitles.get(i).getText();
+            String price = productPrices.get(i).getText();
+            System.out.println((i + 1) + ". " + title + " - " + price);
+        }
+
+	}
+
+}
---
+++ src/test/java/automationtesting/SauceDemoTesting.java
@@ -0,0 +1,78 @@
+package automationtesting;
+import java.time.Duration;
+import org.jspecify.annotations.Nullable;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+public class SauceDemoTesting {
+	WebDriver driver;
+	@BeforeMethod
+	public void launchapp(){
+		//getting the browser page
+		driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/");
+		}
+	@Test
+	public void titleverification() {
+		@Nullable
+		//getting the page title
+		String pagetitle = driver.getTitle();
+		System.out.println("PAGE TITLE IS  "+ pagetitle);
+	}
+	 @Test(dataProvider = "filtereditem")
+	public void testingFilterandAddItemtocart(String filterOption) throws InterruptedException   {
+		  //Login using provided credentials.
+		  driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		  driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		  driver.findElement(By.id("login-button")).click();
+		  Thread.sleep(3000);
+		  //drop down option  price (low to high)
+		  Select dropdown = new Select(driver.findElement(By.className("product_sort_container")));
+		   dropdown.selectByVisibleText(filterOption);
+		   //driver.findElement(By.xpath("//button[text()='OK']")).click();
+		   Thread.sleep(3000);
+		   //Getting the cheapest product
+		    WebElement cheapproduct = driver.findElements(By.className("inventory_item")).get(0);
+	        String productname = cheapproduct.findElement(By.className("inventory_item_name")).getText();
+	        String productprice = cheapproduct.findElement(By.className("inventory_item_price")).getText();
+            // Adding to cart
+	        cheapproduct.findElement(By.tagName("button")).click();
+	        // click on cart button
+	        driver.findElement(By.className("shopping_cart_link")).click();       
+	        WebElement cartItem = driver.findElement(By.className("cart_item"));
+	        String cartprdname = cartItem.findElement(By.className("inventory_item_name")).getText();
+	        String cartprdprice = cartItem.findElement(By.className("inventory_item_price")).getText();
+	        String cartprdquantity = cartItem.findElement(By.className("cart_quantity")).getText();
+	        //verifying the product name,price,item and the quantity.
+	        SoftAssert softAssert = new SoftAssert();
+	        softAssert.assertEquals(cartprdname, productname);
+	        softAssert.assertEquals(cartprdprice, productprice);
+	        softAssert.assertEquals(cartprdquantity, "1");
+	        softAssert.assertAll();
+	        System.out.println("Matching name- " + cartprdname + " : " + productname);
+	        System.out.println("Matching price- " + cartprdprice + " : " + productprice );
+	        System.out.println("Matching quantity- " + cartprdquantity);
+	        System.out.println("The filter item by" + filterOption);
+		}
+	 @DataProvider(name = "filtereditem")
+	    public Object[][] filtertheoption() {
+	        return new Object[][] {
+	            {"Price (low to high)"},
+	            {"Price (high to low)"},
+	            {"Name (A to Z)"}
+	        };
+	 }
+	 public void closeapp() {
+		 driver.quit(); 
+	            System.out.println("Browser closed after test");
+	        
+	    }
+}
